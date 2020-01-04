3cd10cc632a3936b98d11dc510f535f6250cfa7a
Sherief Farid <sherief@farid.cloud>
2020-01-04 21:20:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,11 +14,21 @@ end
 
 def long_planeteer_calls(calls_array)
   calls_array.any? { |word| word.length > 4  }
-  # Your code here
 end
 
-def find_the_cheese(chees_array)
+def find_the_cheese(cheese_array)
 
+  cheese_array.each do |item|
+    counter = 0
+    while counter < cheese_types.length 
+      if cheese_types == item
+        return item
+      end
+      counter +=1
+    end
+      
+    
+  end
 
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
