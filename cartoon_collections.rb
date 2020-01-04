c836778341056c9175583cf21658b02fd494a0ad
Sherief Farid <sherief@farid.cloud>
2020-01-04 21:15:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,9 @@ def long_planeteer_calls(calls_array)
   # Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(chees_array)
+
+  
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
