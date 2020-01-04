782618546f8cc09e4db7689bcf157773a639de7a
Sherief Farid <sherief@farid.cloud>
2020-01-04 21:12:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(array)
-  array.map do |word| 
-    new = word.capitalize  
+  array.map do |word|
+    new = word.capitalize
     new = new + "!"
   end
 end
